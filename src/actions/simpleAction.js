ba9a6a07a45d185a6fc556b37096e040390d351d
FahadAaly <ali-fahad1234@hotmail.com>
2021-03-03 14:15:09 +0500

    hhtp request

--- src/actions/simpleAction.js
+++ src/actions/simpleAction.js
@@ -1,7 +1,17 @@
+import { request } from "../http-helper";
+
 export const simpleAction = (value) => dispatch => {
     console.log("valueeeee", value);
     dispatch({
         type: 'SIMPLE_ACTION',
         payload: value
     })
+}
+
+export const getLists = () => {
+    return(dispatch) => {
+        return request("/list", "get", "").then((res) => {
+            dispatch({ type: "GET_LIST", payload: res.data})
+        }) 
+    }
 }
